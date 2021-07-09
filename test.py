8d483a6e815a4f596ade9671ec816679fc91f4f2
Dominic Greenslade <greenslade.dominic@gmail.com>
2021-07-09 17:26:10 +0100

    Add data methods for SDTF tables in Python module

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
 ### Test bed for function written elsewhere
 
+import sdtfv4
+
+# Using simple function
+#print(sdtfv4.sdtf_e())
+
+# Using inherited class attributes
+#test = sdtfv4.SdtfV4()
+print(sdtfv4.SdtfV4().a())
